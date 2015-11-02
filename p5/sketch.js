8e777a5905485752f45971cd673db5a2b202b181
roschittko <ro.schittko@gmail.com>
2015-11-02 12:08:24 +0000

    adding homepage

---
+++ p5/sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	// place your drawing code here
+	
+stroke("#baf8ff")
+strokeWeight(10);
+fill("#47fff8")
+ellipse(width/2,height/2,150,150);
+ellipse(width/3,height/2,200,150);
+
+quad(15,15,10,200,70,51,90,40);
+
+strokeWeight(7);
+line(width/2,height/2,0,0);
+stroke("green");
+line(300,360,0,0);
+
+rect(200,10,100,100);
+
+triangle(80,80,120,300,160,140,50,90);
+
+point(300,300);
+
+
+noFill();
+stroke(255, 102, 0);
+curve(5, 26, 5, 26, 73, 24, 73, 61);
+stroke(0);
+curve(5, 26, 73, 24, 73, 61, 15, 65);
+stroke(255, 102, 0);
+curve(73, 24, 73, 61, 15, 65, 15, 65);
+
+
+
+
+
+
+
+
+}
+

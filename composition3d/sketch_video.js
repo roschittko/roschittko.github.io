f8f9f6f1c66d66639606fb4924f6b4d007175366
roschittko <ro.schittko@gmail.com>
2015-11-07 20:23:36 +0000

    test

--- composition3d/sketch_video.js
+++ composition3d/sketch_video.js
@@ -1,8 +1,45 @@
-function setup(){
-  setCanvas(windowWidth,windowHeight);
+var img;
+var img2;
+var input;
+var analyzer;
 
+var scale = 0.1;
+var maxH = 0;
+
+var threshold = 0.15;
+var counter = 0;
+
+function setup() {
+  img = loadImage("lib/222.jpg");
+  img2 = loadImage("lib/111.jpg")
+  createCanvas(windowWidth, windowHeight);
+
+  background(0);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
-function draw(){
-  background(255,0,255);
+function draw() {
+  background(0);
+  image(img2, 0, 0, windowWidth, windowHeight);
+  tint(255, counter);
+  image(img, 0, 0, windowWidth, windowHeight);
+
+  var vol = mic.getLevel();
+  var v = map(vol, 0, scale, 255, 0);
+  var mask = counter;
+
+  if (vol >= threshold) {
+    counter++;
+  }
+
+  if (vol <= 0.025) {
+    counter *= 0.95;
+    if (counter <= 0) {
+      counter = 0;
+    }
+  }
+
+  println(mask + " VOLUME: " + vol * 100);
+
 }
